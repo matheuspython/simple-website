50f392eeb109d3cb39ca94189a64f74158810fbd
matheus souza <ruivorobado@gmail.com>
2020-11-07 17:12:51 -0300

    programming responsive menu

--- js/functions.js
+++ js/functions.js
@@ -1,3 +1,19 @@
 $(function() {
+    openWindow();
+    checkClickClose()
 
+    function openWindow() {
+        $('.container-mobile').click(function() {
+            $('.container-mobile ul').fadeIn();
+
+        });
+    }
+
+    function checkClickClose() {
+        var el = $('.section-top');
+        el.click(function() {
+            $('.container-mobile ul').fadeOut();
+        })
+
+    }
 })
